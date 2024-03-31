d7b807edb4c9b5c8e362ec220266f7d693f396bb
MoxForever <moxforever10@gmail.com>
2024-03-31 20:55:09 +0600

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-# Install
-apt install python3
-pip3 install totp_auth
+# Set variables
+PROJECT_DIR="/home/$(whoami)/.config/"
+VENV_DIR="${PROJECT_DIR}/venv"
 
-# Systemd service
+# Create project directory
+mkdir -p "$PROJECT_DIR"
+cd "$PROJECT_DIR" || exit
+
+# Create virtual environment
+python3 -m venv "$VENV_DIR"
+
+# Activate virtual environment
+source "$VENV_DIR/bin/activate"
+
+# Install TOTP Auth
+pip install totp-auth
+
+# Create systemd service file
 cat <<EOF | sudo tee /etc/systemd/system/totp-auth.service > /dev/null
 [Unit]
 Description=TOTP Auth Service
@@ -12,8 +25,8 @@ After=network.target
 
 [Service]
 User=$(whoami)
-WorkingDirectory=/home/$(whoami)/.config/totp-auth
-ExecStart=python3 -m totp-auth run
+WorkingDirectory=$PROJECT_DIR
+ExecStart=$VENV_DIR/bin/totp-auth run
 Restart=always
 RestartSec=3
 
@@ -21,6 +34,7 @@ RestartSec=3
 WantedBy=multi-user.target
 EOF
 
-systemctl daemon-reload
-systemctl enable totp-auth
-systemctl start totp-auth
+# Reload systemd and start the service
+sudo systemctl daemon-reload
+sudo systemctl enable totp-auth
+sudo systemctl start totp-auth
